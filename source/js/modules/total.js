78c1a69a23689e9d6c1c98ff54954799f622d253
Vitya <82025167+ViktorBelenov@users.noreply.github.com>
2023-09-05 04:40:38 +0300

    total

--- source/js/modules/total.js
+++ source/js/modules/total.js
@@ -10,25 +10,29 @@ const prettierPrice = (total) => {
   return totalArray.join('');
 };
 
+
+
 const updateTotalPrice = () => {
 
-  const totalField = document.querySelector('.total__amount');
-
-  const discountField = document.querySelector('.total__discount');
+  const totalAmountField = document.querySelector('.total__amount-product');
+  const totalField = document.querySelector('.total__price');
+  const discountField = document.querySelector('.total__without-discount');
   const cards = document.querySelectorAll('.card');
 
   let total = 0;
   let oldPrice = 0;
+  let amount = 0;
 
   cards.forEach((element) => {
     let id = element.dataset.id;
     if (element.querySelector('.checkbox__input').checked) {
       total = total + CARDS[id - 1].amount * CARDS[id - 1].actualPrice;
       oldPrice = oldPrice + CARDS[id - 1].amount * CARDS[id - 1].oldPrice;
+      amount = amount + CARDS[id - 1].amount;
     }
   });
-  prettierPrice(total);
-  prettierPrice(oldPrice);
+
+  totalAmountField.textContent = amount;
   totalField.textContent = prettierPrice(Math.ceil(total));
   discountField.textContent = prettierPrice(Math.ceil(oldPrice));
 };
