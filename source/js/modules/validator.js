17649672de91f0362f9e179ce012aa01c83b8f42
Vitya <82025167+ViktorBelenov@users.noreply.github.com>
2023-09-12 15:29:53 +0300

    validation

--- source/js/modules/validator.js
+++ source/js/modules/validator.js
@@ -15,7 +15,44 @@ const emailContainer = form.querySelector('.recipient__email-container');
 const phoneInput = form.querySelector('.recipient__number');
 const phoneContainer = form.querySelector('.recipient__number-container');
 
+const innInput = form.querySelector('.recipient__id');
+const innContainer = form.querySelector('.recipient__id-container');
+
+const innDescription = form.querySelector('.recipient__id-description');
+
+const raisesPlaceholder = (container) => {
+  container.querySelector('.recipient__input-placeholder').style.top = '-20px';
+};
+
+const letDownPlaceholder = (container) => {
+  container.querySelector('.recipient__input-placeholder').style.top = '0px';
+};
+
+form.addEventListener('click', (event) => {
+  if (event.target.closest('input')) {
+    raisesPlaceholder(event.target.closest('.recipient__input-container'));
+  }
+});
+
+const phonePritier = () => {
+  let value = phoneInput.value.split();
+  value.splice(1, 0, '');
+  value.splice(5, 0, '');
+  value.splice(9, 0, '');
+  value.splice(12, 0, '');
+  return value.join(' ');
+};
+
+const addErrorClass = (input) => {
+  input.classList.add('recipient__error');
+};
+
+const removeErrorClass = (input) => {
+  input.classList.remove('recipient__error');
+};
+
 const showErrorMessageValidaton = (container) => {
+  raisesPlaceholder(container);
   container.querySelector('.recipient__error-message').classList.remove('visually-hidden');
 };
 const hideErrorMessageValidation = (container) => {
@@ -45,7 +82,16 @@ const checkPhone = () => {
   } else {
     phoneContainer.querySelector('.recipient__error-message').textContent = 'Формат: +9 999 999 99 99';
   }
-  return /^\+\d{1,3} \d{1,3} \d{1,3} \d{1,2} \d{1,2}$/.test(phoneInput);
+  return /^\+\d{1,3}\d{1,3}\d{1,3}\d{1,2}\d{1,2}$/.test(phoneInput.value);
+};
+
+const checkInn = () => {
+  if (innInput.value.length === 0) {
+    innContainer.querySelector('.recipient__error-message').textContent = 'Укажите ИНН';
+  } else {
+    innContainer.querySelector('.recipient__error-message').textContent = 'Проверьте ИНН';
+  }
+  return /^\d{14}$/.test(innInput.value);
 };
 
 
@@ -54,12 +100,15 @@ const customValidation = () => {
   let surname = false;
   let email = false;
   let phone = false;
+  let inn = false;
 
   if (!checkName()) {
     showErrorMessageValidaton(nameContainer);
+    addErrorClass(nameInput);
     nameInput.addEventListener('change', () => {
       if (checkName()) {
         hideErrorMessageValidation(nameContainer);
+        removeErrorClass(nameInput);
       }
     });
   } else {
@@ -68,9 +117,11 @@ const customValidation = () => {
 
   if (!checkSurname()) {
     showErrorMessageValidaton(surnameContainer);
+    addErrorClass(surnameInput);
     surnameInput.addEventListener('change', () => {
       if (checkSurname()) {
         hideErrorMessageValidation(surnameContainer);
+        removeErrorClass(surnameInput);
       }
     });
   } else {
@@ -79,9 +130,11 @@ const customValidation = () => {
 
   if (!checkEmail()) {
     showErrorMessageValidaton(emailContainer);
+    addErrorClass(emailInput);
     emailInput.addEventListener('change', () => {
       if (checkEmail()) {
         hideErrorMessageValidation(emailContainer);
+        removeErrorClass(emailInput);
       }
     });
   } else {
@@ -90,29 +143,40 @@ const customValidation = () => {
 
   if (!checkPhone()) {
     showErrorMessageValidaton(phoneContainer);
+    addErrorClass(phoneInput);
     phoneInput.addEventListener('change', () => {
-      if (checkEmail()) {
+      if (checkPhone()) {
         hideErrorMessageValidation(phoneContainer);
       }
+      phoneInput.value = phonePritier();
+      removeErrorClass(phoneInput);
     });
   } else {
     phone = true;
   }
 
-
-  return name && surname && email && phone;
-};
-// Добавляем обработчик события на кнопку
-submitButton.addEventListener('click', (event) => {
-  // Проверяем валидность формы
-  if (customValidation()) {
-    // Если форма валидна, можно выполнить действие, например, отправить данные на сервер
-    console.log('Форма валидна. Данные будут отправлены.');
+  if (!checkInn()) {
+    showErrorMessageValidaton(innContainer);
+    addErrorClass(innInput);
+    innInput.addEventListener('change', () => {
+      if (checkInn()) {
+        hideErrorMessageValidation(innInput);
+      }
+    });
   } else {
-    // Если форма не валидна, предпримите необходимые действия, например, отобразите сообщение об ошибке
-    console.log('Форма не валидна. Пожалуйста, исправьте ошибки.');
+    inn = true;
   }
 
-  // Предотвращаем отправку формы
+
+  return name && surname && email && phone && inn;
+};
+
+submitButton.addEventListener('click', (event) => {
+  innDescription.classList.add('visually-hidden');
+  if (customValidation()) {
+    console.log('Форма валидна. Данные будут отправлены.');
+  } else {
+    console.log('Форма не валидна. Пожалуйста, исправьте ошибки.');
+  }
   event.preventDefault();
 });
